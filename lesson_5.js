c59737dc5beebd6cb1ad9abaf139aa092b3fd364
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-11 14:10:46 +0100

    add next lesson

---
+++ lesson_5.js
@@ -0,0 +1,32 @@
+function book(title, author) {
+    this.title = title;
+    this.author = author;
+    this.informationAdd = new Array();
+
+    if (typeof book.type == "undefined") {
+
+        book.prototype.information = function () {
+            console.log(`"Tytuł:" ${this.title} "autor:" ${this.author}`)
+        }
+        book.type = true;
+    }
+}
+const book1 = new book("W pustyni i w puszczy", "Henryk Sienkiewicz");
+const book2 = new book("Pan Tadeusz", "Adam Mickiewicz");
+
+book1.informationAdd[0] = "gruba okładka";
+book1.informationAdd[1] = "liczba stron 600";
+
+book2.informationAdd[0] = "brak informacji";
+book2.informationAdd[1] = "liczba stron 400";
+
+book1.information()
+console.log(book1.informationAdd[0])
+console.log(book1.informationAdd[1])
+
+book2.information()
+console.log(book2.informationAdd[0])
+console.log(book2.informationAdd[1])
+
+console.log(book1 instanceof book)
+console.log(book2 instanceof book)
