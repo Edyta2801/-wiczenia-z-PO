5711c0569160e59096f686a0e1d33e70611f6b60
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-12 17:31:30 +0100

    about prototypes in js

---
+++ mpj_prototypes.js
@@ -0,0 +1,29 @@
+function talk(){
+    //console.log(this)
+    console.log(this.sound)
+}
+talk()//undefined
+
+let animal={
+    talk
+}
+let cat={
+    sound:'meow'
+}
+Object.setPrototypeOf(cat, animal)
+cat.talk()//'meow'
+
+let dog={
+    sound:'woof'
+}
+Object.setPrototypeOf(dog, animal)
+dog.talk()
+
+let prerieDog={
+    howl:function(){
+        console.log(this.sound.toUpperCase())
+    }
+}
+Object.setPrototypeOf(prerieDog,dog)
+prerieDog.howl()//'WOOOF'
+
