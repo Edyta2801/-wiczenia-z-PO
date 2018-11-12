02d431859bd97586032927d877a5b8baf9c50968
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-12 15:59:57 +0100

    first lesson about object creation-keyword this

---
+++ mpj_object_creation.js
@@ -0,0 +1,27 @@
+let dog={
+    sound:'woof',
+    talk:function(){
+        console.log(this.sound)
+    }
+}
+dog.talk()
+
+// let talkFunction=dog.talk
+// //talkFunction() //undefined, ponieważ this.sound nie jest przypisany do funkcji
+//  let boundFunction=talkFunction.bind(dog)
+// //połączenie bind powoduje  przypisanie this do dog
+//  boundFunction()//'woof
+
+
+ let button= document.getElementById('MyNiceButton')
+
+ button.addEventListener(
+     'click',
+     dog.talk.bind(dog)
+ )
+console.log(button)
+
+
+
+
+
