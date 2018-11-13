959b6f029ac9a9d4b9fe9a6f1babd5c3e25d0ae6
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-13 22:00:52 +0100

    closures

--- mpj_closures.js
+++ mpj_closures.js
@@ -1,18 +1,10 @@
-// let me='Bruce Wayne'
-// function greetMe(){
-//     console.log('Hello,'+me+'!')
-// }
-
-// me='Batman'
-// greetMe()
-
-
-function sendRequest(){
-    let requestID='123'
-    $.ajax({
-        url:'/myUrl',
-        success:function(response){
-            alert('Request'+requestID+ 'returned')
-        }
-    });
+let me='Bruce Wayne'
+function greetMe(){
+    console.log('Hello,'+me+'!')
 }
+
+me='Batman'
+greetMe()
+
+
+
